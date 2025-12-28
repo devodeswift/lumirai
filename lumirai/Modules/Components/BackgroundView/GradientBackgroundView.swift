e86ec0275f6122ff66a12eb18a4ebfcf70513575
dana nur fiqi <danufiq98@gmail.com>
2025-12-28 16:28:27 +0700

    feat: base reflectionview

--- lumirai/Modules/Components/BackgroundView/GradientBackgroundView.swift
+++ lumirai/Modules/Components/BackgroundView/GradientBackgroundView.swift
@@ -32,3 +32,7 @@ struct GradientBackgroundView: View {
         }
     }
 }
+
+#Preview {
+    GradientBackgroundView()
+}
---
+++ lumirai/Modules/Screens/Reflection/ReflectionView.swift
@@ -0,0 +1,21 @@
+//
+//  ReflectionView.swift
+//  lumirai
+//
+//  Created by dana nur fiqi on 28/12/25.
+//
+
+import Foundation
+import SwiftUI
+
+struct ReflectionView: View {
+    
+    var body: some View {
+        GradientBackgroundView()
+            .ignoresSafeArea()
+    }
+}
+
+#Preview {
+    ReflectionView()
+}
