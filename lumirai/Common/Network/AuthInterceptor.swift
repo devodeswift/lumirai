71fac5b916136d0fb9135460726cce02318c7a72
devodeswift <taufiq.sofa@gmail.com>
2026-04-01 05:13:27 +0700

    fix: change API key gemini

--- lumirai/Common/Network/AuthInterceptor.swift
+++ lumirai/Common/Network/AuthInterceptor.swift
@@ -12,8 +12,7 @@ final class AuthInterceptor: RequestInterceptor {
     func adapt(_ urlRequest: URLRequest, for session: Session, completion: @escaping (Result<URLRequest, Error>) -> Void ) {
         var request = urlRequest
         //set header
-        request.setValue("AIzaSyCOUC2c0YIpMHiDB-FjcVJEAwSfqFe55wI", forHTTPHeaderField: "x-goog-api-key")
-//        request.setValue("AIzaSyAx4-7ED0ANbsbYwrNJH53SIrznQWUXFsM", forHTTPHeaderField: "x-goog-api-key")
+        request.setValue("AIzaSyDBTlWvjMXMhTIFvUNYx2SJZX_fpDRLmDc", forHTTPHeaderField: "x-goog-api-key")
         request.setValue("application/json", forHTTPHeaderField: "Content-Type")
         completion(.success(request))
     }
